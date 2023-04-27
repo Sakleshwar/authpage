2761cffd7c3678d6a5e87a23440a48f60c44e067
Sakleshwar <sakleshwarmarolix@gmail.com>
2023-04-27 07:34:09 +0530

    authpage

---
+++ myproject/authentication/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Invalid username or password."
+    else:
+        error_message = None
+    return render(request, 'login.html', {'error_message': error_message})
